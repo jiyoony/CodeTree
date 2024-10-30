85e1141f01f97f45136adf090f19607db015f60c
jiyoony <65219386+jiyoony@users.noreply.github.com>
2024-10-30 13:38:46 +0900

    Title: 이진수 뒤집기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241030/이진수 뒤집기 2/binary-flip-2.py
+++ 241030/이진수 뒤집기 2/binary-flip-2.py
@@ -12,20 +12,25 @@ def turn(this,idx,change):
         if this == target:
             min_change = min(min_change,change)
         return
+    if change >= min_change:
+        return
+
     #이번걸 바꾸고 넘어가는 경우
-    string = change_binary(this[:],idx) # 새로운 배열을 줘야함
-    turn(string,idx+1,change+1)
+    change_binary(this,idx) # 새로운 배열을 줘야함
+    turn(this,idx+1,change+1)
+    # 원 상태로 복구
+    change_binary(this,idx)
     # 이번걸 건너뛰는 경우
     turn(this,idx+1,change)
 
-def change_binary(string,idx):
-    string[idx] = '0' if string[idx] == '1' else '1'
+def change_binary(this,idx):
+    this[idx] = '0' if this[idx] == '1' else '1'
     if idx != 0:
-        string[idx-1] = '0' if string[idx-1] == '1' else '1'
-    if idx != len(string)-1:
-        string[idx+1] = '0' if string[idx+1] == '1' else '1'
-    return string
+        this[idx-1] = '0' if this[idx-1] == '1' else '1'
+    if idx != n-1:
+        this[idx+1] = '0' if this[idx+1] == '1' else '1'
+    return this
 
 
 turn(cur,0,0)
-print(min_change)
+print(min_change if min_change != float('inf') else -1)
